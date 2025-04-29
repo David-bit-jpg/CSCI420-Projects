7c5e018769582859f033b7f5eadd55a2dac7889f
David-bit-jpg <67268612+David-bit-jpg@users.noreply.github.com>
2025-04-28 18:28:04 -0700

    delete code

--- HW1/assign1_coreOpenGL_starterCode/build/test/gtx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/davidwang/Documents/CSCI420/HW1/assign1_coreOpenGL_starterCode/external/glm/test/gtx
-# Build directory: /Users/davidwang/Documents/CSCI420/HW1/assign1_coreOpenGL_starterCode/build/test/gtx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- HW3/build/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/davidwang/Documents/CSCI420/HW3/hw3-starterCode/external/glm/glm
-# Build directory: /Users/davidwang/Documents/CSCI420/HW3/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
